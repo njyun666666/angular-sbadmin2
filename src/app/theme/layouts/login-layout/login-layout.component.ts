7cdee204dd250fa06539686b89e6cbc2ac252eb7
jyun <njyun666666@gmail.com>
2021-10-28 15:20:38 +0800

    login-layout 的body新增class

--- src/app/theme/layouts/login-layout/login-layout.component.ts
+++ src/app/theme/layouts/login-layout/login-layout.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, OnDestroy, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-login-layout',
   templateUrl: './login-layout.component.html',
   styleUrls: ['./login-layout.component.css']
 })
-export class LoginLayoutComponent implements OnInit {
+export class LoginLayoutComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private renderer: Renderer2
+  ) { }
 
   ngOnInit(): void {
+
+    this.renderer.addClass(this.document.body, 'bg-gradient-primary');
+
   }
+
+
+  ngOnDestroy(): void {
+    this.renderer.removeClass(this.document.body, 'bg-gradient-primary');
+  }
+
 
 }
